1a9065d7902a79f893443fb0d8833c5e8ab1fc93
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-07-19 13:26:01 -0300

    feat: adiciona o mapeamento do dado CPF na tela de alterar

--- FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
+++ FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
@@ -126,7 +126,8 @@ namespace WebAtividadeEntrevista.Controllers
                     Nacionalidade = cliente.Nacionalidade,
                     Nome = cliente.Nome,
                     Sobrenome = cliente.Sobrenome,
-                    Telefone = cliente.Telefone
+                    Telefone = cliente.Telefone,
+                    CPF = cliente.CPF
                 };
 
             
